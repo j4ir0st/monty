e012083d3805d22c00be8cc8a60ce337a94f3dd9
j4ir0st <jairocast2@gmail.com>
2022-01-14 17:29:41 -0800

    Creating match file because of betty style

--- helper_functs.c
+++ helper_functs.c
@@ -1,48 +1,6 @@
 #include "monty.h"
 
 /**
- * match - Matches the input with the stored string
- * @stack: double linked list - stack
- * @tok_0: pointer to structure
- * @line_count: line of command being executed
- * Return: the result of the functions called or -1 on failure
- */
-
-int match(stack_t **stack, char *tok_0, unsigned int line_count)
-{
-	int pos = 0, s_c = 0;
-
-	instruction_t match_store[] = {
-	    {"push", fpush},
-	    {"pall", fpall},
-	    {"pint", fpint},
-	    {"pop", fpop},
-	    {"swap", fswap},
-	    {"add", fadd},
-	    {"nop", fnop},
-	    {"sub", fsub},
-	    {"div", fdiv},
-	    {"mul", fmul},
-	    {"mod", f1mod},
-	    {"pchar", fpchar},
-	    {"pstr", fpstr},
-	    {"rotl", frotl},
-	    {"rotr", frotr},
-	    {NULL, NULL}};
-	while (match_store[pos].opcode)
-	{
-		s_c = strcmp(tok_0, match_store[pos].opcode);
-		if (s_c == 0)
-		{
-			match_store[pos].f(stack, line_count);
-			return (1);
-		}
-		pos++;
-	}
-	return (0);
-}
-
-/**
  * check_isnumber - ckecks if the string represents an integer
  * @is_number: the string represents an integer
  * Return: length on success, 0 on failure
